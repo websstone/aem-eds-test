48ae2564cc982eec67754b9627e0016b6615620a
Siarhei_Kamenchuk <Siarhei.Kamenchuk@travelandleisure.com>
2024-09-28 14:22:44 -0400

    init preact

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,26 @@
 // import { getMetadata } from '../../scripts/aem.js';
 // import { loadFragment } from '../fragment/fragment.js';
+import { render } from 'preact-render-to-string';
+import { h, Component } from 'preact';
+/** @jsx h */
+
+// Classical components work
+class Fox extends Component {
+  render({ name }) {
+    return <span class="fox">{name}</span>;
+  }
+}
+
+// ... and so do pure functional components:
+const Box = ({ type, children }) => (
+  <div class={`box box-${type}`}>{children}</div>
+);
+
+let html = render(
+  <Box type="open">
+    <Fox name="Finn" />
+  </Box>
+);
 
 /**
  * loads and decorates the header
@@ -17,4 +38,5 @@ export default async function decorate(block) {
   row.classList.add('hero-body');
   row.children[0].classList.add('hero-content');
   row.children[0].firstElementChild.style.color = 'yellow';
+  row.append(html);
 }
