7d1a788b611eec7e625d81574df1274063f122fb
Siarhei_Kamenchuk <Siarhei.Kamenchuk@travelandleisure.com>
2024-09-28 20:48:56 -0400

    try htm

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,40 +1,26 @@
 // import { getMetadata } from '../../scripts/aem.js';
 // import { loadFragment } from '../fragment/fragment.js';
-import { render, h } from '../../scripts/preact.js';
+import { render, h, Component } from '../../scripts/preact.js';
 import htm from '../../scripts/htm.js';
-import Fox from './box.js';
 
 const html = htm.bind(h);
-//
-// function App(props) {
-//   return `<h1>Hello ${props.name}!</h1>`;
-// }
-// import { html } from 'htm';
 
-// const Header = ({ name }) => h`<h1>${name} List</h1>`;
-// const Footer = (props) => h`<footer ...${props} />`;
-//
-// class App extends Component {
-//   addTodo() {
-//     const { todos = [] } = this.state;
-//     this.setState({ todos: todos.concat(`Item ${todos.length}`) });
-//   }
-//
-//   render({ page }, { todos = [] }) {
-//     return h`
-//           <div class="app">
-//             <${Header} name="ToDo's (${page})" />
-//             <ul>
-//               ${todos.map((todo) => h`
-//                 <li key=${todo}>${todo}</li>
-//               `)}
-//             </ul>
-//             <button onClick=${() => this.addTodo()}>Add Todo</button>
-//             <${Footer}>footer content here<//>
-//           </div>
-//         `;
-//   }
-// }
+class Fox extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  render() {
+    if (this.state.loading) {
+      return htm`<div>Loading...</div>`;
+    }
+    return htm`<h1>FOX Component ${this.props.name}</h1>`;
+  }
+}
 
 /**
  * loads and decorates the header
@@ -53,9 +39,7 @@ export default async function decorate(block) {
   row.children[0].classList.add('hero-content');
   row.children[0].firstElementChild.style.color = 'yellow';
 
-  // const html = render(
-  //   <App name="World" />
-  // );
-  const app = html`<${Fox} name='Billy' />`;
+  const name = 'Billy';
+  const app = html`<${Fox} name=${name} />`;
   render(app, block);
 }
